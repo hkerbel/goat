f03df1ddd51991f364f8ae4aa57f457c85b53b3d
Hart <hart.kerbel@sympatico.ca>
2018-04-11 18:36:59 -0400

    Added URL for adding new list items.

--- lists/views.py
+++ lists/views.py
@@ -3,16 +3,17 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
-
     return render(request, 'home.html')     ### GET
 
 
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html', {'items': items})     ### GET
+
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
 
 
 def OBSOLETEhome_page(request):
