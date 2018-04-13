af7b6e3667eb71b01dd678399d3342fd4b5a09ae
Hart <hart.kerbel@sympatico.ca>
2018-04-13 02:01:12 -0400

    End of chapter 7. Copied lists/tests.py and lists/views.py from book's git. Had an error in one of them (now resolved)

--- lists/views.py
+++ lists/views.py
@@ -1,14 +1,8 @@
 from django.shortcuts import redirect, render
 from lists.models import Item, List
 
-# Create your views here.
 def home_page(request):
-    return render(request, 'home.html')     ### GET
-
-
-def view_list(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})     ### GET
+    return render(request, 'home.html')
 
 
 def new_list(request):
@@ -16,16 +10,15 @@ def new_list(request):
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
 
+
 def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}')
+    return redirect(f'/lists/{list_.id}/')
+    ###return redirect('/lists/%d/' % (list_.id,))
 
 
-def OBSOLETEhome_page(request):
-    if request.method == 'POST':
-        ###[hjk:1 - Code in book chapter 5.3 is flawed - must render via template. Apr 9 2018 ]
-        ###[hjk:1] return HttpResponse(request.POST['item_text'])
-        return TemplateResponse(request, 'home.html', {'new_item_text': request.POST['item_text']})
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    return render(request, 'list.html', {'list': list_})
 
-    return render(request, 'home.html')     ### GET
