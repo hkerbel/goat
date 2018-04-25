ac170a408f8eec8a8da3645ccfdd2c42c8114d83
Hart <hart.kerbel@sympatico.ca>
2018-04-25 03:18:34 -0400

    End of chapter 14 - TDD Testing goat. All is working well.

--- lists/tests/test_forms.py
+++ lists/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 
 from lists.forms import EMPTY_ITEM_ERROR, ItemForm
+from lists.models import List, Item
 
 
 class ItemFormTest(TestCase):
@@ -26,4 +27,13 @@ class ItemFormTest(TestCase):
     def test_home_page_uses_item_form(self):
         response = self.client.get('/')
         self.assertIsInstance(response.context['form'], ItemForm)
-        
+
+
+    def test_form_save_handles_saving_to_a_list(self):
+        list_ = List.objects.create()        
+        form = ItemForm(data={'text': 'do me'})        
+        new_item = form.save(for_list=list_)
+        self.assertEqual(new_item, Item.objects.first())
+        self.assertEqual(new_item.text, 'do me')
+        self.assertEqual(new_item.list, list_)
+
--- lists/views.py
+++ lists/views.py
@@ -12,23 +12,10 @@ def new_list(request):
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
-        Item.objects.create(text=request.POST['text'], list=list_)
+        form.save(for_list=list_)
         return redirect(list_)
     else:
         return render(request, 'home.html', {'form': form})
-
-    # # # list_ = List.objects.create()    
-    # # # item = Item.objects.create(text=request.POST['text'], list=list_)
-    # # # ####item = Item.objects.create(text=request.POST['item_text'], list=list_)
-    
-    # # # try:
-    # # #     item.full_clean()
-    # # # except ValidationError:
-    # # #     list_.delete()
-    # # #     error = "You can't have an empty list item"
-    # # #     return render(request, 'home.html', {'error': error})
-    
-    # # # return redirect(list_)      # Utilizes get_absolute_url.
 
 
 def view_list(request, list_id):
@@ -37,7 +24,7 @@ def view_list(request, list_id):
     if request.method == 'POST':
         form = ItemForm(data=request.POST)
         if form.is_valid():
-            Item.objects.create(text=request.POST['text'], list=list_)
+            form.save(for_list=list_)
             return redirect(list_)
 
     return render(request, 'list.html', {'list': list_, 'form': form})
