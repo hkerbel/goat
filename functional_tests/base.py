f84622f373f0bc3f71e00a6eeaa3b30cab7c93b0
Hart <hart.kerbel@sympatico.ca>
2018-04-30 14:22:46 -0400

    Cleaned up wait_to_be_logged_in(). Invalid debug statements (my bad).

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -69,8 +69,7 @@ class FunctionalTest(StaticLiveServerTestCase):
 
     @wait
     def wait_to_be_logged_in(self, email):
-        debugELEMENT = self.browser.find_element_by_link_text('Log out')
-        print('>>>>> debugELEMENT:', debugELEMENT)
+        self.browser.find_element_by_link_text('Log out')
         navbar = self.browser.find_element_by_css_selector('.navbar')
         self.assertIn(email, navbar.text)
 
